a5086adf2e9565f88dede42d3c728020f95e8004
UV <Yuvaraj.Arunachalam@outlook.com>
2025-11-18 22:14:48 +0530

    Fix timedelta import

--- src/drift_detection/continuous_monitor.py
+++ src/drift_detection/continuous_monitor.py
@@ -8,7 +8,7 @@ import sys
 import time
 import numpy as np
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 from supabase import create_client, Client
 from datadog import initialize, statsd
 import json
